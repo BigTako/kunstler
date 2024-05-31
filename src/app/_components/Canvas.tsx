b3393eb1a7350fa3dcb19a9ecc88a44724c26fcb
BigTako <libert761@gmail.com>
2024-05-31 13:49:27 +0300

    [MID-8040210489] integrated canvas as template

---
+++ src/app/_components/Canvas.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React, { useRef } from 'react';
+
+export function Canvas(props: object) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  return <canvas className="z-[-1] h-full w-full" ref={canvasRef} {...props} />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { HeaderToolbar } from './_components/HeaderToolbar';
 import { AsideToolbar } from './_components/AsideToolbar';
+import { Canvas } from './_components/Canvas';
 
 export default function Home() {
   return (
     <main className="relative h-full w-full">
+      <Canvas />
       <HeaderToolbar />
       <AsideToolbar />
     </main>
